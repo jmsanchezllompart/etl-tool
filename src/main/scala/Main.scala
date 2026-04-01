6d7cba83139e64e4051b03894c027a6717fe94d3
jmsanchezllompart <josemariasanchezllompart106@gmail.com>
2026-04-01 13:46:04 +0200

    Experiment with Scopt 4.1.0

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,51 +1,62 @@
-import io.circe.yaml
-import parser.pipeline.DataPipelineParser
+import scopt.OParser
+import java.io.File
 
+case class Config
+(
+  foo: Int = -1,
+  out: File = new File("."),
+  configFile: String = "",
+  libName: String = "",
+  maxCount: Int = -1,
+  verbose: Boolean = false,
+  debug: Boolean = false,
+  mode: String = "",
+  files: Seq[File] = Seq(),
+  keepalive: Boolean = false,
+  jars: Seq[File] = Seq(),
+  kwargs: Map[String, String] = Map()
+)
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val json = yaml.parser.parse(
-    """
-      Metadata:
-        DisplayName: "Postgres To BigQuery Pipeline"
-        Description: "Sample pipeline that reads data from a PostgreSQL database and writes it to BigQuery"
-        Id: "pg2bq_1"
-      DataSource:
-        PostgresSqlSource:
-          Host: "localhost"
-          Port: 5432
-          Database: "Postgres"
-          Auth:
-            BasicAuth:
-              User: "admin"
-              Password: "root"
-          RawQuery: "SELECT user_id, user_email, order_id, order_date, item_price, item_id, item_quantity FROM order_items;"
-      Transformations:
-        - RenameColumn:
-            OldColumnName: "user_id"
-            NewColumnName: "user"
-        - RenameColumn:
-            OldColumnName: "user_email"
-            NewColumnName: "email"
-      DataSink:
-        BigQuerySink:
-          Project: "test-project"
-          Dataset: "test-dataset"
-          Table: "test-table"
-          Auth:
-            ServiceAccountAuth:
-              ServiceAccountFile: "opt/shared/service_accounts/testing_service_accounts.json"
-          AppendMode:
-            Overwrite
-    """)
-    val scalaJson = json match {
-      case Right(json) => json
-      case Left(_) => throw new Exception
+    val builder = OParser.builder[Config]
+    val parser1 = {
+      import builder._
+      OParser.sequence(
+        programName("etl-tool"),
+        head("etl-tool", "0.1.0-SNAPSHOT"),
+        help("help").text("prints this usage text"),
+        note("some notes." + sys.props("line.separator")),
+        cmd("run")
+          .action((_, c) => c.copy(mode = "run"))
+          .text("run is the command used to run the pipelines specified in the config file")
+          .children(
+            opt[String]("config-file")
+              .abbr("cf")
+              .action((x, c) => c.copy(configFile = x))
+              .text("Config file where the pipelines are stored")
+              .required(),
+          ),
+        cmd("check")
+          .action((_, c) => c.copy(mode = "run"))
+          .text("check is the command used to check if the provided config file has a valid format")
+          .children(
+            opt[String]("config-file")
+              .abbr("cf")
+              .action((x, c) => c.copy(configFile = x))
+              .text("Config file where the pipelines are stored")
+              .required(),
+          ),
+      )
     }
 
-    val root = scalaJson.hcursor
-
-    val pipeline = DataPipelineParser.parse(root)
+    // OParser.parse returns Option[Config]
+    OParser.parse(parser1, args, Config()) match {
+      case Some(config) =>
+      // do something
+      case _ =>
+      // arguments are bad, error message will have been displayed
+    }
   }
 }
 
